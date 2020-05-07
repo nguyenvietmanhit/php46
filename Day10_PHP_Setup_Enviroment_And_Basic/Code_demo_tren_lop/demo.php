10382ced2ad758ed3cff2a0e937eb76c5258e599
Nguyen viet manh <nguyenvietmanhit@gmail.com>
2020-05-07 21:31:05 +0700

    update

---
+++ Day10_PHP_Setup_Enviroment_And_Basic/Code_demo_tren_lop/demo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf8" />
+    <title>Demo PHP</title>
+</head>
+<body>
+    <h1>PHP đầu tiên</h1>
+
+    <?php
+    //viết code php ở đây
+    echo 'Hello World';
+
+    echo date('d-m-Y H:i:s');
+    ?>
+
+</body>
+</html>
