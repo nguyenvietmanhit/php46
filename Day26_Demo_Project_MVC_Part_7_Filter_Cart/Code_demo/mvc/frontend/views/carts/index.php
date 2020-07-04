9cf2098254bcf46ed68ba069ca077e17ceb81df9
Manh <nvmanh@cmc.com.vn>
2020-07-04 22:43:22 +0700

    update

---
+++ Day26_Demo_Project_MVC_Part_7_Filter_Cart/Code_demo/mvc/frontend/views/carts/index.php
@@ -0,0 +1,76 @@
+<div class="timeline-items container">
+    <h2>Giỏ hàng của bạn</h2>
+    <form action="" method="post">
+        <table class="table table-bordered">
+            <tbody>
+            <tr>
+                <th width="40%">Tên sản phẩm</th>
+                <th width="12%">Số lượng</th>
+                <th>Giá</th>
+                <th>Thành tiền</th>
+                <th></th>
+            </tr>
+
+            <tr>
+                <td>
+                    <img class="product-avatar img-responsive" src="../backend/assets/uploads/1588432245-product-hang1edit.jpg" width="80">
+                    <div class="content-product">
+                        <a href="chi-tiet-san-pham/samsung-s9/5" class="content-product-a">
+                            đâs11111111111111 </a>
+                    </div>
+                </td>
+                <td>
+                    <!--                      cần khéo léo đặt name cho input số lượng, để khi xử lý submit form update lại giỏ hàng sẽ đơn giản hơn    -->
+                    <input type="number" min="0" name="9" class="product-amount form-control" value="1">
+                </td>
+                <td>
+                    111,111,111                      </td>
+                <td>
+                    111,111,111                      </td>
+                <td>
+                    <a class="content-product-a" href="xoa-san-pham/9">
+                        Xóa
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <img class="product-avatar img-responsive" src="../backend/assets/uploads/1588432245-product-hang1edit.jpg" width="80">
+                    <div class="content-product">
+                        <a href="chi-tiet-san-pham/samsung-s9/5" class="content-product-a">
+                            2 </a>
+                    </div>
+                </td>
+                <td>
+                    <!--                      cần khéo léo đặt name cho input số lượng, để khi xử lý submit form update lại giỏ hàng sẽ đơn giản hơn    -->
+                    <input type="number" min="0" name="1" class="product-amount form-control" value="1">
+                </td>
+                <td>
+                    111,111,111                      </td>
+                <td>
+                    111,111,111                      </td>
+                <td>
+                    <a class="content-product-a" href="xoa-san-pham/1">
+                        Xóa
+                    </a>
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="5" style="text-align: right">
+                    Tổng giá trị đơn hàng:
+                    <span class="product-price">
+                                            222,222,222 vnđ
+                                                </span>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="5" class="product-payment">
+                    <input type="submit" name="submit" value="Cập nhật lại giá" class="btn btn-primary">
+                    <a href="thanh-toan" class="btn btn-success">Đến trang thanh toán</a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </form>
+</div>
